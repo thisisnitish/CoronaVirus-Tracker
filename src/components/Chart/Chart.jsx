873029ea0fd1e357e93f6e368e14e7784ec884e8
Nitish Kumar <nitishkumargenius@gmail.com>
2020-08-09 01:19:31 +0530

    Refactored the lines

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -16,21 +16,26 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
     const lineChart = (
         dailyData.length ? (
             <Line
-                data = {{
-                    labels: dailyData.map(({ date }) => date),
-                    datasets: [{
-                        data: dailyData.map(({ confirmed }) => confirmed),
-                        label: 'Infected',
-                        borderColor: '#3333ff',
-                        fill: true,
-                    }, {
-                        data: dailyData.map(({ deaths }) => deaths),
-                        label: 'Deaths',
-                        borderColor: 'red',
-                        backgroundColor: 'rgba(255, 0, 0, 0.5)',
-                        fill: true,
-                    }],
-                }}
+                data = {
+                    {
+                        labels: dailyData.map(({ date }) => date),
+                        datasets: [
+                            {
+                                data: dailyData.map(({ confirmed }) => confirmed),
+                                label: 'Infected',
+                                borderColor: '#3333ff',
+                                fill: true,
+                            }, 
+                            {
+                                data: dailyData.map(({ deaths }) => deaths),
+                                label: 'Deaths',
+                                borderColor: 'red',
+                                backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                                fill: true,
+                            },
+                        ],
+                    }
+                }
             />
         ) : null
     );
