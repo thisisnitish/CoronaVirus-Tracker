699dc099b3595f3b24f476f915c3122d629f8499
Nitish Kumar <nitishkumargenius@gmail.com>
2020-08-08 22:43:54 +0530

    refactored the code in api

--- src/api/index.js
+++ src/api/index.js
@@ -13,25 +13,24 @@ export const fetchData = async (country) => {
        return { confirmed, recovered, deaths, lastUpdate };
    }
    catch(error){
-        console.log(error);
+       return error;
    } 
-}
+};
 
 export const fetchDailyData = async () => {
     try{
         const { data } = await axios.get(`${URL}/daily`);
         
-        const modifiedData = data.map((dailyData) => ({
-            confirmed: dailyData.confirmed.total,
-            deaths: dailyData.deaths.total,
-            date: dailyData.reportDate
+        return data.map(({ confirmed, deaths, reportDate: date }) => ({
+            confirmed: confirmed.total,
+            deaths: deaths.total,
+            date: date
         }));
-        return modifiedData;
     }
     catch(error){
-
+        return error;
     }
-}
+};
 
 export const fetchCountries = async () => {
     try{
@@ -39,6 +38,6 @@ export const fetchCountries = async () => {
         return countries.map((country) => country.name);
     }
     catch(error){
-        console.log(error);
+        return error;
     }
-}
+};
