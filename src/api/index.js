12007f44f3e7af58ebff839ab3a082bb62c75227
Nitish Kumar <nitishkumargenius@gmail.com>
2020-08-08 17:27:07 +0530

    Impelemented Chart for Global Data

--- src/api/index.js
+++ src/api/index.js
@@ -16,7 +16,13 @@ export const fetchData = async () => {
 export const fetchDailyData = async () => {
     try{
         const { data } = await axios.get(`${URL}/daily`);
-        console.log(data);
+        
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate
+        }));
+        return modifiedData;
     }
     catch(error){
 
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
 
 const CountryPicker = () => {
     return(
